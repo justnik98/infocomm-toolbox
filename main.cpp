cbb817824da297864e427c6f0bb1ca58d2d1be48
Nikita Yankovskii <yannik98@yandex.ru>
2022-04-17 13:21:34 +0300

    added timer

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <chrono>
+#include <iomanip>
 #include "channels/awgnchannel.hpp"
 
 using namespace  std;
 int main() {
-    vector<float> v (100000);
+    vector<float> v (100000000);
     AWGN_channel c;
-    v = c.add_noise(v,3);
+    auto start = std::chrono::system_clock::now();
+    v = c.add_noise(v,20);
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> diff = end - start;
+    std::cout << "Time to fill and iterate a vector of " << std::setw(9)
+              << v.size() << ": " << diff.count() << " s\n";
+
     auto sum = 0.0;
     for(auto &&vi: v){
         sum += vi*vi;
